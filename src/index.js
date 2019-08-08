3e16945e60f807d32366dd25fc82b2f9b42de2d5
Vadim Veksler <reyzele91@gmail.com>
2019-08-08 11:39:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RootRouter from './components/RootRouter';
+import './index.css';
+
+ReactDOM.render(<RootRouter />, document.getElementById('root'));
